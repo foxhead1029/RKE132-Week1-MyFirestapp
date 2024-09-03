2969dcf9c6b4890cf978a657c266f0945cba822e
Hanni@PaulLaptop <hannipaulhenning@gmail.com>
2024-09-03 23:22:50 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// topelt murd joonega saab panna teksti mis ei ilmu programmi jooksutamise ajal
+
+Console.WriteLine("Enter your name:");//output
+//String-sõne,ning mitte sõna
+
+string userName = Console.ReadLine();//input
+
+Console.WriteLine("Hello "+userName);//output
+
+//string interpolation(21. sajandi süsteem)
+Console.WriteLine($"Hello, {userName} !");//output
